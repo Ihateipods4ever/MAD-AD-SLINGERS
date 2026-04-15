fc23f396c49afd25a08ffe0888297d7d9a2a65a4
Ihateipods4ever <eatmyshortsson@gmail.com>
2026-04-15 06:32:36 -0500

    Fix CORS

--- backend/index.py
+++ backend/index.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://frontend-mu-tan-45.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
